a91ab107fa99e1d74b27d6d46b2f0fde627c97dd
Stefan Dörig <sdoerig@bluewin.ch>
2021-03-23 20:39:20 +0100

    Moved to sync and insert_many

--- database_bouncycastle/src/bouncycastle.rs
+++ database_bouncycastle/src/bouncycastle.rs
@@ -1,39 +1,35 @@
-use mongodb::{bson::doc, options::ClientOptions, Client};
+use mongodb::{bson::doc};
+use mongodb::sync::Client;
 use chrono::prelude::*;
 
-pub fn hello() {
-    println!("hello")
-}
+type InsertManyType = Result<mongodb::results::InsertManyResult, mongodb::error::Error>;
+type VecDocType = Vec<mongodb::bson::Document>;
 
+pub fn poplate_db() -> mongodb::error::Result<()> {
+    let client = Client::with_uri_str("mongodb://127.0.2.15:27017/?w=majority")?;
 
-pub async fn poplate_db() -> mongodb::error::Result<()> {
-    // Parse your connection string into an options struct
-    let mut client_options =
-        ClientOptions::parse("mongodb://127.0.2.15:27017/test?w=majority")
-            .await?;
-
-    // Manually set an option
-    client_options.app_name = Some("Rust Demo".to_string());
-
-    // Get a handle to the cluster
-    let client = Client::with_options(client_options)?;
-
-    // Ping the server to see if you can connect to the cluster
-    client
-        .database("admin")
-        .run_command(doc! {"ping": 1}, None)
-        .await?;
-    println!("Connected successfully.");
-
-    // List the names of the databases in that cluster
-    let nestbox = client.database("nestbox");
-    let breeds = nestbox.collection("breeds");
-    for _i in  0..10000 {
-        let _result = breeds.insert_one(doc!{"name": format!("breed_eleven {}", _i), "date": Utc::now()}, None).await?;
-        //println!("{:#?}", result.inserted_id.as_object_id().unwrap());
+    let database = client.database("nestbox");
+    let breeds = database.collection("breeds");
+    
+    let mut breed_docs: VecDocType = Vec::new();
+    for i in  0..100000 {
+        breed_docs.push(doc!{"name": format!("breed_eleven {}", i), "date": Utc::now()});
+        if i % 10000 == 0 {
+            let _result = write_to_db(&breeds, &mut breed_docs)?;
+            for (id, object_id) in _result.inserted_ids {
+                println!("id {:#?}, object_id {:#?}", id, object_id);
+            }
+            
+        } 
     } 
-    for db_name in client.list_database_names(None, None).await? {
-        println!("{}", db_name);
+    if breed_docs.len() > 0 {
+        let _result = write_to_db(&breeds, &mut breed_docs)?;
     }
     Ok(())
+}
+
+fn write_to_db(breeds: &mongodb::sync::Collection, breed_docs: &mut VecDocType) -> InsertManyType {
+    let result = breeds.insert_many(breed_docs.clone().into_iter(), None)?;
+    breed_docs.clear();
+    Ok(result)
 }
