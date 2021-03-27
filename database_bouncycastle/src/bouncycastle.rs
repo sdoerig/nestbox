a8ec13c492f411518343d6201dab3d90a0dd1108
Stefan Dörig <sdoerig@bluewin.ch>
2021-03-27 15:51:52 +0100

    Some more - before refactoring.

--- database_bouncycastle/src/bouncycastle.rs
+++ database_bouncycastle/src/bouncycastle.rs
@@ -1,6 +1,7 @@
 use mongodb::{bson::doc};
 use mongodb::sync::Client;
 use chrono::prelude::*;
+use uuid::Uuid;
 
 type InsertManyType = Result<mongodb::results::InsertManyResult, mongodb::error::Error>;
 type VecDocType = Vec<mongodb::bson::Document>;
@@ -16,11 +17,18 @@ pub fn poplate_db(db_uri: &str, records_to_insert: i32) -> mongodb::error::Resul
         
     let mut docs: VecDocType = Vec::new();
     docs.push(doc!{"name": "BirdLife",  "website": "https://www.birdwatcher.ch", "email": "bird@iseeyou.ch"});
-    let _result = write_to_db(&mandant, &mut docs)?;
-    println!("mandant {:#?}", _result);
-    let mandant_object = _result.inserted_ids.get(&0).unwrap();
+    let mut mandant_result = write_to_db(&mandant, &mut docs)?;
+    println!("mandant {:#?}", mandant_result);
+    let mut mandant_object = mandant_result.inserted_ids.get(&0).unwrap();
     for i in 0..records_to_insert {
-        docs.push(doc!{"public": true});
+        let uuid_4 = Uuid::new_v4().to_string(); 
+
+        docs.push(doc!{"public": true, "uuid": uuid_4, "mandant": mandant_object});
+        if i % 100 == 0 {
+            let mut mandant_docs: VecDocType = vec![doc!{"name": format!("BirdLife {}", i),  "website": "https://www.birdwatcher.ch", "email": "bird@iseeyou.ch"}];
+            mandant_result = write_to_db(&mandant, &mut mandant_docs)?;
+            mandant_object = mandant_result.inserted_ids.get(&0).unwrap();
+        }
         if i % STEP_SIZE == 0 {
             let _result = write_to_db(&nestboxes, &mut docs);
         }
@@ -32,7 +40,7 @@ pub fn poplate_db(db_uri: &str, records_to_insert: i32) -> mongodb::error::Resul
 
 
     for i in  0..records_to_insert {
-        docs.push(doc!{"name": format!("breed_eleven {}", i), "date": Utc::now(), "mandant": mandant_object});
+        docs.push(doc!{"name": format!("breed_eleven {}", i), "date": Utc::now()});
         if i % STEP_SIZE == 0 {
             let _result = write_to_db(&breeds, &mut docs)?;
             //for (id, object_id) in _result.inserted_ids {
