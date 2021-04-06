cf68e3271755ca221029a27317f30fcd743cb39a
Stefan Dörig <sdoerig@bluewin.ch>
2021-04-06 22:02:45 +0200

    Loading yaml

---
+++ nestboxd/src/extract_argv.rs
@@ -0,0 +1,70 @@
+use getopts::Options;
+use std::fs;
+use std::process;
+use std::{env, string};
+use yaml_rust::{YamlEmitter, YamlLoader};
+
+fn print_usage(program: &str, opts: &Options) {
+    let brief = format!("Usage: {} -c CONFIG_FILE", program);
+    print!("{}", opts.usage(&brief));
+}
+
+pub fn extract_argv() -> String {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optopt("c", "config", "Path to configuration file", "CONFIG_FILE");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(_f) => {
+            print_usage(&program, &opts);
+            process::exit(2)
+        }
+    };
+    let config_file_path = match matches.opt_str("c") {
+        Some(m) => m,
+        None => {
+            print_usage(&program, &opts);
+            process::exit(3)
+        }
+    };
+    config_file_path
+}
+
+pub struct Config {
+    pub mongodb_uri: String,
+    pub mongodb_database: String,
+    pub httpserver_ip: String,
+    pub httpserver_port: String,
+}
+
+pub fn parse_yaml(config_file: String) -> Config {
+    let config_str = match std::fs::read_to_string(&config_file) {
+        Ok(s) => s,
+        Err(_e) => process::exit(4),
+    };
+    let config_yaml = YamlLoader::load_from_str(&config_str).unwrap();
+    let config_doc = &config_yaml[0];
+    Config {
+        mongodb_uri: config_doc["mongodb"]["uri"].as_str().unwrap().to_string(),
+        mongodb_database: config_doc["mongodb"]["database"]
+            .as_str()
+            .unwrap()
+            .to_string(),
+        httpserver_ip: config_doc["httpserver"]["ip"].as_str().unwrap().to_string(),
+        httpserver_port: config_doc["httpserver"]["port"].as_str().unwrap().to_string(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    //#[test]
+    fn test_config() {
+        let config = parse_yaml(String::from("../../nestboxd_conf.yaml"));
+        assert_eq!(config.httpserver_ip, String::from("127.0.0.1"));
+    }
+}
