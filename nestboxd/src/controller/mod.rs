0653fa809551a317157d04c333cb0b002a57ccec
Stefan Dörig <sdoerig@bluewin.ch>
2021-04-11 22:00:21 +0200

    Reformated

--- nestboxd/src/controller/mod.rs
+++ nestboxd/src/controller/mod.rs
@@ -8,47 +8,63 @@ pub struct NestboxReq {
 
 #[derive(Serialize, Deserialize)]
 struct NestboxResponse {
-    uuid: String
+    uuid: String,
 }
-
 
 #[get("/nestboxes/{uuid}")]
 pub async fn nestboxes_get(
     app_data: web::Data<crate::AppState>,
     nestbox: web::Path<NestboxReq>,
 ) -> impl Responder {
-    let result =
-        web::block(move || app_data.service_container.nestbox.get_by_uuid(&nestbox.uuid)).await;
+    let result = web::block(move || {
+        app_data
+            .service_container
+            .nestbox
+            .get_by_uuid(&nestbox.uuid)
+    })
+    .await;
     match result {
         Ok(doc) => HttpResponse::Ok().json(doc),
-        Err(_e) => {
-            HttpResponse::NotFound().finish()
-        }
+        Err(_e) => HttpResponse::NotFound().finish(),
     }
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct LoginReq {
     pub email: String,
-    pub password: String
+    pub password: String,
 }
 #[derive(Deserialize, Serialize)]
 pub struct LoginRes {
     pub email: String,
     pub password: String,
-    pub badword: String
+    pub badword: String,
 }
 
 #[post("/login")]
-pub async fn login_post(app_data: web::Data<crate::AppState>, 
-    login: web::Json<LoginReq>) -> impl Responder {
-    let password_hash = app_data.service_container.user.login(&login.email, &login.password).await;
+pub async fn login_post(
+    app_data: web::Data<crate::AppState>,
+    login: web::Json<LoginReq>,
+) -> impl Responder {
+    let password_hash = app_data
+        .service_container
+        .user
+        .login(&login.email, &login.password)
+        .await;
     match password_hash {
-        Some(r) => return HttpResponse::Ok().json(LoginRes{ email: login.email.clone(), 
-            password: login.password.clone(), badword: r}),
-        None => return HttpResponse::Ok().json(LoginRes{ email: login.email.clone(), 
-            password: login.password.clone(), badword: String::from("jackass")})
+        Some(r) => {
+            return HttpResponse::Ok().json(LoginRes {
+                email: login.email.clone(),
+                password: login.password.clone(),
+                badword: r,
+            })
+        }
+        None => {
+            return HttpResponse::Ok().json(LoginRes {
+                email: login.email.clone(),
+                password: login.password.clone(),
+                badword: String::from("jackass"),
+            })
+        }
     };
-    
-
 }
--- nestboxd/src/service/mod.rs
+++ nestboxd/src/service/mod.rs
@@ -49,8 +49,8 @@ impl UserService {
             None => return None,
         };
 
-        let mut pw_hash_salt: Vec<String>  = Vec::new();
-        
+        let mut pw_hash_salt: Vec<String> = Vec::new();
+
         for key in &["password_hash", "salt"] {
             let bson = userobj.get(key);
             let string = match bson {
@@ -59,21 +59,24 @@ impl UserService {
             };
             pw_hash_salt.push(String::from(string));
         }
-        if is_password_correct(password, &pw_hash_salt.get(0).unwrap(), &pw_hash_salt.get(1).unwrap()) {
-            return Some(String::from("password_is_correct"))
+        if is_password_correct(
+            password,
+            &pw_hash_salt.get(0).unwrap(),
+            &pw_hash_salt.get(1).unwrap(),
+        ) {
+            return Some(String::from("password_is_correct"));
         }
         Some(String::from("password_is_incorrect"))
     }
 }
 
 fn is_password_correct(password: &str, password_hash: &str, salt: &str) -> bool {
-    
     let mut hasher = Sha3_256::new();
     let password_with_salt = format!("{}_{}", password, salt);
     hasher.update(password_with_salt);
     let password_hash_result = hex::encode(hasher.finalize());
     if password_hash_result == password_hash {
         return true;
-    } 
+    }
     false
 }
