46357a1399dfb18ac0a51aba3429710ea8fac632
Stefan Dörig <sdoerig@bluewin.ch>
2021-05-08 20:48:18 +0200

    Fixed breeds service

--- nestboxd/src/controller/breed.rs
+++ nestboxd/src/controller/breed.rs
@@ -3,29 +3,17 @@ use actix_web::{get, web, HttpResponse, Responder};
 use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct BreedReq {
-    uuid: String,
+    pub uuid: String,
 }
 
 #[get("/nestboxes/{uuid}/breeds")]
 pub async fn breeds_get(
     app_data: web::Data<crate::AppState>,
-    nestbox: web::Path<BreedReq>,
+    breed_req: web::Path<BreedReq>,
 ) -> impl Responder {
-    let breed_col = BreedService::new(app_data.service_container.db.collection("breeds"));
 
-    let nestbox_result = web::block(move || {
-        app_data
-            .service_container
-            .nestbox
-            .get_by_uuid(&nestbox.uuid)
-    })
-    .await;
-    let nestbox = match nestbox_result {
-        Ok(doc) => doc.unwrap(),
-        Err(_e) => return HttpResponse::NotFound().finish(),
-    };
 
-    let breeds = breed_col.get_by_nestbox(&nestbox).await;
+    let breeds = app_data.service_container.breed.get_by_nestbox_uuid(&breed_req.uuid).await;
 
     HttpResponse::Ok().json(breeds)
 }
