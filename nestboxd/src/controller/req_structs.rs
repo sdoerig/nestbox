8a879a67436a2c29792a5fc12b7bb06794d88ebc
Stefan Dörig <sdoerig@bluewin.ch>
2021-06-22 19:30:43 +0200

    Added draft geolocations

--- nestboxd/src/controller/req_structs.rs
+++ nestboxd/src/controller/req_structs.rs
@@ -10,3 +10,11 @@ pub struct BirdReq {
     pub bird_uuid: String,
     pub bird: String
 }
+
+#[derive(Deserialize)]
+pub struct GeolocationReq {
+    pub long: f32,
+    pub lat: f32
+}
+
+
--- nestboxd/src/service/geolocation.rs
+++ nestboxd/src/service/geolocation.rs
@@ -1,5 +1,5 @@
 use bson::doc;
-use chrono::Utc;
+use chrono::{Utc, Duration};
 use uuid::Uuid;
 
 use super::service_helper as sa;
@@ -9,7 +9,6 @@ use crate::controller::{
 };
 use crate::controller::{req_structs::NestboxReq, utilities::SessionObject};
 use mongodb::{error::Error, Collection};
-
 
 #[derive(Clone)]
 pub struct GeolocationService {
@@ -21,15 +20,35 @@ impl GeolocationService {
         GeolocationService { collection }
     }
 
-    pub async fn post_geolocation(&self, nestbox_uuid: &str) {
+    pub async fn post_geolocation(&self, nestbox_uuid: &str, long: f32, lat: f32) {
         // End current location before entering a new one.
         let now = Utc::now();
-        match self.collection.update_many(doc! {"nestbox_uuid": nestbox_uuid, "until_date": {"$gt": &now}}, 
-        doc! {"$set": {"until_date": &now}}, None).await {
+        match self
+            .collection
+            .update_many(
+                doc! {"nestbox_uuid": nestbox_uuid, "until_date": {"$gt": &now}},
+                doc! {"$set": {"until_date": &now}},
+                None,
+            )
+            .await
+        {
             Ok(_) => todo!(),
             Err(_) => todo!(),
         };
+        match self
+            .collection
+            .insert_one(
+                doc! { "uuid" : "3490e91f-8d0a-4a7e-94a0-ccd17d9832ab",
+                "nestbox_uuid" : nestbox_uuid, "from_date" : &now,
+                "until_date" : Utc::now() + Duration::days(365000),
+                "position" : { "type" : "point", "coordinates" : [ &long, &lat ] } },
+                None,
+            )
+            .await
+        {
+            Ok(_) => todo!(),
+            Err(_) => todo!(),
+        }
         //db.geolocations.updateMany({"nestbox_uuid": "eb0c7048-2cda-471d-beb3-7777b7d54858", "until_date": {"$lt": new ISODate("2021-06-12T20:15:31Z")}}, {$set: {"until_date": new ISODate("2021-06-12T20:15:31Z")}})
-
     }
 }
